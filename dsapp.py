cc60bc3c2a921b4f5e6b1024e56e4e9aa7b52eef
snielson <snielson@projectuminfinitas.com>
2016-03-29 23:22:59 -0600

    3.29.16
    * Fixed no 'version' attribute in dsUpdate()
    * Fixed --config restore switch to properly start Mobility during the
    connector setup
    * Fixed dsUpdate() to reload variables for any script change (1.x to
    2.x)
    * Added option to hide spinner on mobility start/stop/restart with
    rcDS()
    * Added option to hide prints with rcDS()

--- dsapp.py
+++ dsapp.py
@@ -14,7 +14,7 @@ __credits__ = "Tyler Harris"
 __maintainer__ = "Shane Nielson"
 __email__ = "snielson@projectuminfinitas.com"
 
-dsappversion='233'
+dsappversion='234'
 
 ##################################################################################################
 #	Imports
