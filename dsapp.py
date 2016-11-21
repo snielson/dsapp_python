17343ac4f4150207d8695a378456dd22667c7bdd
snielson <snielson@projectuminfinitas.com>
2016-11-21 15:44:10 -0700

    Version bump for v242 // release

--- dsapp.py
+++ dsapp.py
@@ -14,7 +14,7 @@ __credits__ = "Tyler Harris"
 __maintainer__ = "Shane Nielson"
 __email__ = "snielson@projectuminfinitas.com"
 
-dsappversion='241'
+dsappversion='242'
 
 ##################################################################################################
 #	Imports
